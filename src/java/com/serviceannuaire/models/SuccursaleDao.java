15fff6c4a76e2199f65daba08a59c4826fa844a2
LordVoldemor <31547757+LordVoldemor@users.noreply.github.com>
2018-02-26 10:48:37 -0500

    Commit Final
    - Creation de la dao
    - Creation de la connexion
    - Creation d'un fichier de test

--- src/java/com/serviceannuaire/models/SuccursaleDao.java
+++ src/java/com/serviceannuaire/models/SuccursaleDao.java
@@ -5,10 +5,76 @@
  */
 package com.serviceannuaire.models;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author Avasam
  */
 public class SuccursaleDao {
+    
+    protected Connection cnx;
+    
+    public SuccursaleDao(Connection cnx) {
+        this.cnx = cnx;
+    }
+    
+    public List<Succursale> findAll() {
+        List<Succursale> liste = new LinkedList();
+        
+        try
+        {
+           Statement stm = cnx.createStatement();
+           ResultSet r = stm.executeQuery("SELECT * FROM Succursales");
+           while(r.next())
+           {
+                Succursale s = new Succursale();
+                s.setNoEntrepriseQuebec(r.getInt("NOENTREPRISEQUEBEC"));
+                s.setNom(r.getString("NOM"));
+                s.setLattitude(r.getFloat("LATTITUDE"));
+                s.setLongitude(r.getFloat("LONGITUDE"));
+                s.setDescription(r.getString("DESCRIPTION"));
+                liste.add(s);
+           }
+           r.close();
+           stm.close();
+        }
+        catch(SQLException exp)
+        {
+        
+        }
+        return liste;
+    }
+    public List<Succursale> findByDistance(int Distance) {
+        List<Succursale> liste = new LinkedList();
+        
+        /*try
+        {
+           Statement stm = cnx.createStatement();
+           ResultSet r = stm.executeQuery("SELECT * FROM Succursales");
+           while(r.next())
+           {
+                Succursale s = new Succursale();
+                s.setNoEntrepriseQuebec(r.getInt("NOENTREPRISEQUEBEC"));
+                s.setNom(r.getString("NOM"));
+                s.setLattitude(r.getFloat("LATTITUDE"));
+                s.setLongitude(r.getFloat("LONGITUDE"));
+                s.setDescription(r.getString("DESCRIPTION"));
+                liste.add(s);
+           }
+           r.close();
+           stm.close();
+        }
+        catch(SQLException exp)
+        {
+        
+        }*/
+        return liste;
+    }
 
 }
